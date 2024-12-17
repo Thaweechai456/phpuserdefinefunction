52bb99d05367f5cef82c560b4e37217dd820bbd9
Woraluethai <66040233102@udru.ac.th>
2024-12-17 10:41:44 +0700

    update

--- udf.php
+++ udf.php
@@ -9,6 +9,40 @@
     <?php
     $name = "นายทวีชัย นามฮาด";
     Hello($name);
+    //-------------------------------------------------
+    echo "<h3>ทดสอบการใช้ Function</h3>";
+    $a = 5;
+    $b = 8;
+    $c = sum($a,$b);
+    echo "$a + $b = $c <br>";
+    echo "<hr>";
+    echo "<h3>ทดสอบการใช้ Function แบบมีพารามิเตอร์เป็น Reference</h3>";
+    $num = 2;
+    echo "Before ===> \$num = $num <br>";
+    add_5($num);
+    add_5($num);
+    echo "After ===> \$num = $num";
+    echo "<hr>";
+    echo "<h3>ทดสอบการใช้ Function แบบมีพารามิเตอร์หลายตัว<h3>";
+    $summaryNumber = sumMyNumber(1,2,3,4,5,6,7,8,9);
+    echo "ผลรวมของ (1,2,3,4,5,6,7,8,9) = $summaryNumber";
+    echo "<hr>";
+
+    //-------------------------------------------------
+    bye();
+    function sumMyNumber(...$x){
+        $sum=0;
+        foreach ($x as $value) {
+            $sum += $value;
+        }
+        return;
+    }
+
+
+    function add_5(&$value){
+        $value+=5;
+    }
+    
 
     function Hello($yourname){
         echo "<h3>ผู้พัฒนาโปรแกรม</h3>";
@@ -17,12 +51,13 @@
     }
     function sum($x,$y) {
         $z = $x+$y;
-        return
+        return $z;
     }
     function bye(){
         echo "<hr>";
         echo "<h4>หลักสูตรวิทยาศาตร์บัณฑิต เทคโนโลยีสารสนเทศ</h4>";
         echo "<h4>คณะวิทยาศาตร์ มหาวิทยาลัยราชภัฏอุดรธานี</h4>";
+        echo "<hr>";
     }
     ?>
 </body>
